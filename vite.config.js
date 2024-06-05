6ef2bf2e52b16b1c553ff90577c56b77178fa227
Raphitpt <tiphonetraphi@gmail.com>
2024-06-05 10:38:18 +0200

    chore: Update dependencies and enable HTTPS server
    This commit updates the dependencies in the package.json file, adding "node-fetch", "react-geocode", "react-speech-recognition", and "regenerator-runtime". It also enables the HTTPS server in the vite.config.js file by setting the `https` option to `true`.
    
    Note: This message does not include the recent user commits or repository commits.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { https: true },
   plugins: [
     react(),
     VitePWA({
