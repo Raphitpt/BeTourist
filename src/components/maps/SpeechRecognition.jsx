fb5965e5fc84854a6b52ccab77799e5d22ab1004
Raphitpt <tiphonetraphi@gmail.com>
2024-06-19 12:28:33 +0200

    Fix sur la recherche vocale + Fin du projet

--- src/components/maps/SpeechRecognition.jsx
+++ src/components/maps/SpeechRecognition.jsx
@@ -1,16 +1,10 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import { Micro } from "../../assets/icon/Icon";
 
-const SpeechRecognition = (props) => {
-  const [transcript, setTranscript] = useState("");
+const SpeechRecognition = ({ onInterimResult }) => {
   const [isListening, setIsListening] = useState(false);
-  const [isMicrophoneAccessible, setIsMicrophoneAccessible] = useState(false);
-  const [audioLevel, setAudioLevel] = useState(0);
-  const audioContextRef = useRef(null);
-  const analyserRef = useRef(null);
-  const microphoneRef = useRef(null);
 
   useEffect(() => {
-    // Vérifiez si l'API Web Speech Recognition est disponible
     const SpeechRecognition =
       window.SpeechRecognition || window.webkitSpeechRecognition;
     if (!SpeechRecognition) {
@@ -21,7 +15,7 @@ const SpeechRecognition = (props) => {
     }
 
     const recognition = new SpeechRecognition();
-    recognition.continuous = true;
+    recognition.continuous = false;
     recognition.interimResults = true;
     recognition.lang = "fr-FR"; // Changez la langue si nécessaire
 
@@ -38,11 +32,12 @@ const SpeechRecognition = (props) => {
     recognition.onresult = (event) => {
       let interimTranscript = "";
       for (let i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-          setTranscript((prev) => prev + event.results[i][0].transcript + " ");
-        } else {
+        if (!event.results[i].isFinal) {
           interimTranscript += event.results[i][0].transcript;
         }
+      }
+      if (onInterimResult) {
+        onInterimResult(interimTranscript);
       }
       console.log("Interim result:", interimTranscript);
     };
@@ -60,58 +55,7 @@ const SpeechRecognition = (props) => {
     return () => {
       recognition.stop();
     };
-  }, [isListening]);
-
-  useEffect(() => {
-    // Fonction pour vérifier l'accès au microphone et surveiller les niveaux audio
-    const getMicrophoneAccess = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({
-          audio: true,
-        });
-        setIsMicrophoneAccessible(true);
-
-        // Créez un contexte audio et un analyseur
-        const audioContext = new (window.AudioContext ||
-          window.webkitAudioContext)();
-        const analyser = audioContext.createAnalyser();
-        analyser.fftSize = 256;
-        const source = audioContext.createMediaStreamSource(stream);
-        source.connect(analyser);
-
-        // Sauvegardez les références pour arrêter l'audio plus tard
-        audioContextRef.current = audioContext;
-        analyserRef.current = analyser;
-        microphoneRef.current = stream;
-
-        // Fonction pour obtenir les niveaux audio
-        const getAudioLevels = () => {
-          const dataArray = new Uint8Array(analyser.frequencyBinCount);
-          analyser.getByteFrequencyData(dataArray);
-          const maxLevel = Math.max(...dataArray);
-          setAudioLevel(maxLevel);
-        };
-
-        // Surveillez les niveaux audio à intervalle régulier
-        const interval = setInterval(getAudioLevels, 100);
-        return () => clearInterval(interval);
-      } catch (err) {
-        console.error("Erreur d'accès au microphone : ", err);
-        setIsMicrophoneAccessible(false);
-      }
-    };
-
-    getMicrophoneAccess();
-
-    return () => {
-      if (audioContextRef.current) {
-        audioContextRef.current.close();
-      }
-      if (microphoneRef.current) {
-        microphoneRef.current.getTracks().forEach((track) => track.stop());
-      }
-    };
-  }, []);
+  }, [isListening, onInterimResult]);
 
   const handleButtonClick = () => {
     setIsListening((prevState) => !prevState);
@@ -120,26 +64,11 @@ const SpeechRecognition = (props) => {
   return (
     <div>
       <button onClick={handleButtonClick}>
+        <Micro /> {/* Votre icône Micro */}
         {isListening
-          ? "Arrêter la reconnaissance"
-          : "Démarrer la reconnaissance"}
+          ? " Arrêter la reconnaissance"
+          : " Démarrer la reconnaissance"}
       </button>
-      <div>
-        <h2>Transcription</h2>
-        <p>{transcript}</p>
-      </div>
-      <div>
-        <h2>État du microphone</h2>
-        <p>
-          {isMicrophoneAccessible
-            ? "Microphone accessible"
-            : "Microphone non accessible"}
-        </p>
-      </div>
-      <div>
-        <h2>Niveau audio</h2>
-        <p>{audioLevel}</p>
-      </div>
     </div>
   );
 };
--- src/components/maps/maps.jsx
+++ src/components/maps/maps.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
-import { Loupe, Micro } from "../../assets/icon/Icon";
-import SpeechRecognition from "./SpeechRecognition";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
@@ -12,42 +10,16 @@ const MAPBOX_TOKEN =
 
 const Maps = () => {
   const mapContainerRef = useRef(null);
-  const mapRef = useRef(null);
   const geocoderRef = useRef(null);
-  const [currentQuery, setCurrentQuery] = useState("");
-
-  const geolocateUser = (map) => {
-    const geolocate = new mapboxgl.GeolocateControl({
-      positionOptions: {
-        enableHighAccuracy: true,
-      },
-      fitBoundsOptions: {
-        linear: false,
-      },
-      trackUserLocation: false,
-    });
-    map.addControl(geolocate);
-    map.on("load", function () {
-      geolocate.trigger();
-    });
-    geolocate.on("geolocate", function (e) {
-      map.flyTo({
-        zoom: 15,
-        center: [e.coords.longitude, e.coords.latitude],
-      });
-    });
-  };
 
   useEffect(() => {
     mapboxgl.accessToken = MAPBOX_TOKEN;
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
-      style: "mapbox://styles/mapbox/streets-v12",
+      style: "mapbox://styles/raphititi/clx1tr61a01rg01qs3ycedsti",
       center: [0.15, 45.65],
       zoom: 13,
     });
-
-    mapRef.current = map;
 
     const geocoder = new MapboxGeocoder({
       accessToken: mapboxgl.accessToken,
@@ -60,22 +32,115 @@ const Maps = () => {
 
     map.addControl(geocoder);
     geocoderRef.current = geocoder;
-    geolocateUser(map);
+
     return () => {
       map.remove();
     };
   }, []);
 
+  const injectMicroIcon = () => {
+    const geocoderContainer = document.querySelector(".mapboxgl-ctrl-geocoder");
+    if (geocoderContainer) {
+      const microIconContainer = document.createElement("div");
+      microIconContainer.classList.add("micro-icon-container");
+
+      const microIcon = document.createElement("div");
+      microIcon.classList.add("micro-icon");
+
+      const microIconSvg = document.createElement("div");
+      microIconSvg.innerHTML = `<svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clip-path="url(#clip0_8_56)">
+          <path
+            d="M12 14.6553C10.5769 14.6553 9.42456 13.4522 9.42456 11.9662V5.68902C9.42456 4.2031 10.5769 3 12 3C13.4232 3 14.5755 4.2031 14.5755 5.68902V11.9662C14.5755 13.4522 13.4232 14.6553 12 14.6553Z"
+            fill="black"
+          />
+          <path
+            d="M12.8612 17.7115H11.1429V21H12.8612V17.7115Z"
+            fill="black"
+          />
+          <path
+            d="M16.3019 12C16.2978 14.4737 14.3693 16.4873 12 16.4873C10.8113 16.4873 9.7318 15.9808 8.95148 15.1618L7.75067 16.4156C8.83827 17.5511 10.3383 18.2561 12 18.2561C15.3113 18.2561 17.996 15.4531 18 11.9958H16.3019V12Z"
+            fill="black"
+          />
+          <path
+            d="M8.95148 15.1618C8.17925 14.3513 7.69811 13.2326 7.69811 12H6C6 13.7223 6.67116 15.2842 7.75067 16.4156L8.95148 15.1618Z"
+            fill="black"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_8_56">
+            <rect
+              width="12"
+              height="18"
+              fill="white"
+              transform="translate(6 3)"
+            />
+          </clipPath>
+        </defs>
+      </svg>`;
+
+      microIcon.appendChild(microIconSvg);
+      microIconContainer.appendChild(microIcon);
+      geocoderContainer.appendChild(microIconContainer);
+
+      microIcon.addEventListener("click", startSpeechRecognition);
+    }
+  };
+
+  useEffect(() => {
+    injectMicroIcon();
+  }, []);
+
+  const startSpeechRecognition = () => {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (!SpeechRecognition) {
+      console.error(
+        "Votre navigateur ne supporte pas l'API Web Speech Recognition."
+      );
+      return;
+    }
+
+    const recognition = new SpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
+    recognition.lang = "fr-FR";
+
+    recognition.onresult = (event) => {
+      let interimTranscript = "";
+      for (let i = event.resultIndex; i < event.results.length; ++i) {
+        if (!event.results[i].isFinal) {
+          interimTranscript += event.results[i][0].transcript;
+        }
+      }
+      console.log("Interim result:", interimTranscript);
+      geocoderRef.current.query(interimTranscript);
+    };
+
+    recognition.onerror = (event) => {
+      console.error("Erreur de reconnaissance vocale : " + event.error);
+    };
+
+    recognition.start();
+
+    return () => {
+      recognition.stop();
+    };
+  };
+
   return (
-    <>
+    <div>
       <div
         ref={mapContainerRef}
         style={{ width: "100%", height: "calc(100vh - 80px)" }}
       />
-      <div style={{ position: "absolute", top: 10, left: 10 }}>
-        <SpeechRecognition />
-      </div>
-    </>
+    </div>
   );
 };
 
